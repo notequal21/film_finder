46037cc2dea061f231851863ffd5bc30fe7ee786
Kirill Makhnev <kalorigen@gmail.com>
2023-04-17 17:46:26 +0300

    query changed to post

--- src/store/api.ts
+++ src/store/api.ts
@@ -3,13 +3,18 @@ import axios from 'axios';
 export const fetchResult = (queryData: any) => {
   console.log(queryData.join());
 
-  return axios
-    .get('http://localhost:5000/film_list', {
-      params: {
-        query: queryData.join(),
-      },
-    })
-    .then(function (response) {
-      return response;
-    });
+  return (
+    axios
+      // .get('http://localhost:5000/film_list', {
+      //   params: {
+      //     query: queryData.join(),
+      //   },
+      // })
+      .post('http://localhost:5000/query', {
+        query: queryData,
+      })
+      .then(function (response) {
+        return response;
+      })
+  );
 };
